89ac8474237a5b207fc76e5217cdc533d780491f
HostZero <haneesh.da.rocker@gmail.com>
2016-06-27 10:47:09 +0530

    Removed DT and Dependencies

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,8 +44,7 @@ TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_MKBOOTIMG_ARGS :=  --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --kernel_offset 0x00008000 --second_offset 0x00f00000 --dt device/google/shamrock/prebuilt/dt.img
+BOARD_MKBOOTIMG_ARGS :=  --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --kernel_offset 0x00008000 --second_offset 0x00f00000 --board shamrock
 TARGET_PREBUILT_KERNEL := device/google/shamrock/prebuilt/kernel
 ENABLE_CPUSETS := true
 TARGET_KERNEL_ARCH := arm64
